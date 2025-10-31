89f94bd40395fd48c59e1654d4dddad8edc41901
Arnab Mallick <arnab2239@gmail.com>
2025-10-31 14:40:18 +0530

    Update jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,11 +1,12 @@
-import jwt from 'jsonwebtoken'
+import jwt, { Secret, SignOptions } from "jsonwebtoken";
 
-const SECRET = process.env.JWT_SECRET || 'dev_secret'
+const SECRET = process.env.JWT_SECRET as Secret;
 
 export function signToken(payload: object, expiresIn = '7d') {
-  return jwt.sign(payload, SECRET, { expiresIn })
+  const options: SignOptions = { expiresIn };
+  return jwt.sign(payload, SECRET, options);
 }
 
 export function verifyToken(token: string) {
-  return jwt.verify(token, SECRET) as any
+  return jwt.verify(token, SECRET);
 }
